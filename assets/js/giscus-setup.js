cc8703820de2ee1c55169a8ac75e1228c833cd84
avanturist322 <128804659+avanturist322@users.noreply.github.com>
2025-09-30 15:14:03 +0000

    Deploying to gh-pages from @ avanturist322/avanturist322.github.io@401b50b674bf2ac265109e556fd12999ab871d86 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "avanturist322/avanturist322",
+    "data-repo": "avanturist322/avanturist322.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
