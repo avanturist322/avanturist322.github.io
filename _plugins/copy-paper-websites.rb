dbdd29904ce9962868b800f9926f3a5f57ed3173
Avanturist322 <cherepanovegor2018@gmail.com>
2026-01-21 00:29:12 +0300

    added kagebench template

--- _plugins/copy-paper-websites.rb
+++ _plugins/copy-paper-websites.rb
@@ -3,11 +3,17 @@
 require "fileutils"
 
 Jekyll::Hooks.register :site, :post_write do |site|
-  source_dir = File.join(site.source, "papers_websites", "KAGEBench")
-  next unless Dir.exist?(source_dir)
+  base_source_dir = File.join(site.source, "papers_websites")
+  next unless Dir.exist?(base_source_dir)
 
-  dest_dir = File.join(site.dest, "KAGEBench")
-  FileUtils.rm_rf(dest_dir)
-  FileUtils.cp_r(source_dir, dest_dir)
+  Dir.children(base_source_dir).sort.each do |entry|
+    next if entry.start_with?(".")
+
+    source_dir = File.join(base_source_dir, entry)
+    next unless File.directory?(source_dir)
+
+    dest_dir = File.join(site.dest, entry)
+    FileUtils.rm_rf(dest_dir)
+    FileUtils.cp_r(source_dir, dest_dir)
+  end
 end
-
